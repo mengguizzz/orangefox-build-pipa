731346930ce45c93b027d5c75cd57ddcea969b57
mengguizzz <mengguizzz@users.noreply.github.com>
2026-09-22 15:04:33 +0800

    fix: remove obsolete gsi keys include for fox 14.1

--- twrp_pipa.mk
+++ twrp_pipa.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
